9f5c79b2b13147cdec5b2949d03cbb62f04311bc
jamesb04 <jb9056112@gmail.com>
2023-01-21 17:53:54 +0530

    version 6

---
+++ byte.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class b
+{
+public static void main(String...args)
+{
+byte age;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the age: ");
+age=sc.nextByte();
+System.out.println("Age is: "+age);
+}
+}
---
+++ mul.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class mul
+{
+public static void main(String...x)
+{
+Double a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the 1st no.");
+a=sc.nextDouble();
+System.out.println("Enter the 2nd no.");
+b=sc.nextDouble();
+c=a*b;
+System.out.print("The product is:"+c);
+}
+
+
+
+}
