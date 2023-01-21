cc1be00867e58076f9492ec498234119f0724cb2
jamesb04 <jb9056112@gmail.com>
2023-01-21 17:49:22 +0530

    version 5

--- name1.java
+++
@@ -1,9 +0,0 @@
-//wap to read your name as a command line argument and display on it
-class name1
-{
-public static void main(String...args) {
-System.out.print("Your name is: "+args[0]+"\nRoll no.: "+args[1]+"\nFees: "+args[2]+"/-\n");
-int x=args[0].length();
-System.out.print("The length of your name is: "+x);
-}
-}
